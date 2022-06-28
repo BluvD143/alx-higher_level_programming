95ee5282634a743d8bdedb3ce540662b4539f8c3
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-06-28 22:44:39 +0100

    Task 11

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
-from math import pow
-def pow(a, b):
-    return a ** b
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        i = i - 32
+    print("{}".format(chr(i)), end="")
