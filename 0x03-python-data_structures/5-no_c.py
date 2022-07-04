ef6d6c5b5617713de39503db0d045bee80abaf69
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-07-04 20:42:03 +0100

    Task 5 - removes all characters c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Bello Abayomi
+
+def no_c(my_string):
+    chars = len(my_string)
+    buf = ""
+    for i in range(0, chars):
+        if my_string[i] == "c" or my_string[i] == "C":
+            continue
+        buf = buf + my_string[i]
+    return buf
