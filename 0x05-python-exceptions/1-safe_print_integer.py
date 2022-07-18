c972ff6308dd1b572569d211d3cf8944bf45fcb1
BluvD143 <101479751+BluvD143@users.noreply.github.com>
2022-07-18 16:03:02 +0100

    Task 1 - prints an integer with {:d}.format()

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-safe_print_integer.py
+
+def safe_print_integer(value):
+	"""Print an integer with "{:d}".format().
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception:
+        return False
