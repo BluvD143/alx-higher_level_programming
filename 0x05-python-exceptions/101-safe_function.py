7552086bbf7242f2e858f14e069141f44b28ecad
BELLO ABAYOMI <101479751+BluvD143@users.noreply.github.com>
2022-07-19 05:29:05 +0100

    Task 8 - executes a function safely.

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 # 101-safe_function.py
 
+from __future__ import print_function
+import sys
+
+
 def safe_function(fct, *args):
 	"""Executes a function safely.
     try:
-        r = fct(*args)
-        return r
-    except Exception as error:
-        import sys
-        print("Exception: {}".format(error), file=sys.stderr)
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return None
